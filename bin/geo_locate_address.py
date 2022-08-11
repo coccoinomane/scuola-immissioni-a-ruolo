8515887e865400e69e522a238b80d5c1b606bcc2
Guido W. Pettinari <guido.pettinari@idearia.it>
2022-08-11 20:55:59 +0200

    ✨ Migliorato geo_locate_address

--- bin/geo_locate_address.py
+++ bin/geo_locate_address.py
@@ -5,12 +5,23 @@ readable form, using Google Maps APIs
 Source: https://github.com/vaclavdekanovsky/data-analysis-in-examples/blob/master/Maps/Geocoding/Address%20to%20Location.ipynb
 """
 
+from sys import argv
 from geopy.geocoders import GoogleV3
 from src.common.dotenv import getenv
+from src.libs.general import secondOrNone
 
+# Parse
+address = secondOrNone(argv)
+if not address:
+    raise Exception(
+        "Dammi un indirizzo, usando le virgolette, ad es. '1 Apple Park Way, Cupertino, CA'"
+    )
+
+# Extract
 AUTH_KEY = getenv("GOOGLE_MAPS_API_KEY")
 geolocator = GoogleV3(api_key=AUTH_KEY)
+point = geolocator.geocode(address).point
 
-point = geolocator.geocode("1 Apple Park Way, Cupertino, CA").point
+# Print
 print(f"latitude = {point.latitude}")
 print(f"longitude = {point.longitude}")
